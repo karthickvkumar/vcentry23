c17262bc2667aedb14306ac2116c4f322c93f42b
kvelraj_concord <kvelraj@concord.net>
2022-10-01 12:01:44 +0530

    javascript

---
+++ HTML/program.js
@@ -0,0 +1,25 @@
+// Number -> Any numerical value from Positive, Negative, Fractional, Decimal
+
+// Syntax - To create a Variable (Temp Storage)
+// var variable_name = value;
+
+// variable_name -> Any valid text, it should contains a-z, A-Z, 0-9, _, $
+// value -> user input
+
+var roll_number = 4785;
+console.log(roll_number);
+
+// String -> Any text value which is typed inside ' ' or " "
+var user_email_id = "karthick12@gmail.com";
+console.log(user_email_id);
+
+// Boolean -> Either true or false
+var result = true;
+console.log(result);
+
+// undefined -> Its value will not be defined
+var future_value;
+console.log(future_value);
+
+roll_number = 100;
+console.log(roll_number);
