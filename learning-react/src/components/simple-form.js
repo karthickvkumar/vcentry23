9357d8c29ccd121e575599852824e185db2b21b6
kvelraj_concord <kvelraj@concord.net>
2022-10-23 11:45:53 +0530

    form

--- learning-react/src/components/simple-form.js
+++ learning-react/src/components/simple-form.js
@@ -9,28 +9,33 @@ const SimpleFormSubmition = () => {
   }
 
   // let [variableName, functionName] = useState(intialValue);
-  let [userName, setUserName] = useState("");
+  let [loginForm, setLoginForm] = useState({
+    username : "",
+    password : ""
+  });
   
   const onSubmitForm = () => {
-    alert("Submitted successfully");
+    console.log(loginForm)
   }
 
   const onHandleInput = (event) => {
-    setUserName(event.target.value);
+    // console.log(event.target.value, event.target.id);
+    setLoginForm({...loginForm, [event.target.id] : event.target.value });
   }
 
   return(
     <div>
       <h1>{message}</h1>
       <h2>My current location is {address.street} and {address.area}</h2>
-      <h2>The username is {userName}</h2>
+      <h2>The username is {loginForm.username}</h2>
+      <h2>The password is {loginForm.password}</h2>
       <div className="space">
         <label className="lable">Enter your UserName</label>
-        <input type="text" placeholder="Please enter username.." onChange={onHandleInput} />
+        <input type="text" placeholder="Please enter username.." onChange={onHandleInput} id="username"/>
       </div>
       <div className="space">
         <label className="lable">Enter your Password</label>
-        <input type="password" placeholder="Please enter password" onChange={onHandleInput} />
+        <input type="password" placeholder="Please enter password" onChange={onHandleInput} id="password"/>
       </div>
       <div className="space">
         <button onClick={() => onSubmitForm()}>Submit Form</button>
