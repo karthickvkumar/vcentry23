6248cab73079d0c303c5e2b47aea1356f1b36f4d
kvelraj_concord <kvelraj@concord.net>
2022-10-23 11:14:37 +0530

    useState

--- learning-react/src/components/simple-form.js
+++ learning-react/src/components/simple-form.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 const SimpleFormSubmition = () => {
   
@@ -7,22 +7,30 @@ const SimpleFormSubmition = () => {
     street: "appasamy street",
     area: "washermenpet"
   }
+
+  // let [variableName, functionName] = useState(intialValue);
+  let [userName, setUserName] = useState("");
   
   const onSubmitForm = () => {
     alert("Submitted successfully");
+  }
+
+  const onHandleInput = (event) => {
+    setUserName(event.target.value);
   }
 
   return(
     <div>
       <h1>{message}</h1>
       <h2>My current location is {address.street} and {address.area}</h2>
+      <h2>The username is {userName}</h2>
       <div className="space">
         <label className="lable">Enter your UserName</label>
-        <input type="text" placeholder="Please enter username.." />
+        <input type="text" placeholder="Please enter username.." onChange={onHandleInput} />
       </div>
       <div className="space">
         <label className="lable">Enter your Password</label>
-        <input type="password" placeholder="Please enter password" />
+        <input type="password" placeholder="Please enter password" onChange={onHandleInput} />
       </div>
       <div className="space">
         <button onClick={() => onSubmitForm()}>Submit Form</button>
