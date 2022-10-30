e5b5ae4e162631a544a7f8963c400d6077bbca6b
kvelraj_concord <kvelraj@concord.net>
2022-10-30 12:00:03 +0530

    client side error handling

--- learning-react/src/pages/resiger.js
+++ learning-react/src/pages/resiger.js
@@ -8,17 +8,50 @@ const RegisterPage = () => {
     password : "",
     dateOfBirth : "",
     gender: "",
-    hobbies: "",
+    hobbies: [],
     address: "",
     qualification: ""
   });
 
+  let [formError, setFormError] = useState({
+    userName : false,
+    emailId : false,
+    password : false,
+    dateOfBirth : false,
+    gender: false,
+    hobbies: false,
+    address: false,
+    qualification: false
+  })
+
   const submitRegister = () => {
     console.log(registerForm);
+    setFormError({
+      userName : registerForm.userName === "" ? true : false,
+      emailId : registerForm.emailId === "" ? true : false,
+      password : registerForm.password === "" ? true : false,
+      dateOfBirth : registerForm.dateOfBirth === "" ? true : false,
+      gender : registerForm.gender === "" ? true : false,
+      hobbies : registerForm.hobbies.length === 0 ? true : false,
+      address : registerForm.address === "" ? true : false,
+      qualification : registerForm.qualification === "" ? true : false
+    })
   }
 
   const handleFormInput = (event) => {
-    setRegisterForm({...registerForm, [event.target.name] : event.target.value});
+    if(event.target.name === "hobbies"){
+      if(event.target.checked){
+        registerForm.hobbies.push(event.target.value);
+      }
+      else{
+        const index = registerForm.hobbies.indexOf(event.target.value);
+        registerForm.hobbies.splice(index, 1);
+      }
+      setRegisterForm({...registerForm, hobbies : registerForm.hobbies });
+    }
+    else{
+      setRegisterForm({...registerForm, [event.target.name] : event.target.value});
+    }
   }
 
   return(
@@ -26,25 +59,30 @@ const RegisterPage = () => {
       <h1>Register Page</h1>
       <div className="space">
         <label className="register-lable">Enter your Name :</label>
-        <input type="text" placeholder="Enter your name" className="register-input" onChange={handleFormInput} name="userName"/>
+        <input type="text" placeholder="Enter your name" className={formError.userName ? "register-input input-error" : "register-input"} onChange={handleFormInput} name="userName"/>
+        { formError.userName && <p className="error">Please enter a valid user name</p>}
       </div>
       <div className="space">
         <label className="register-lable">Enter your Email ID :</label>
-        <input type="text" placeholder="Enter your email id" className="register-input" onChange={handleFormInput} name="emailId"/>
+        <input type="text" placeholder="Enter your email id" className={formError.emailId ? "register-input input-error" : "register-input"} onChange={handleFormInput} name="emailId"/>
+        {formError.emailId && <p className="error">Please enter a valid email id</p>}
       </div>
       <div className="space">
         <label className="register-lable">Enter your Password :</label>
-        <input type="password" placeholder="Enter your password" className="register-input" onChange={handleFormInput} name="password"/>
+        <input type="password" placeholder="Enter your password"  className={formError.password ? "register-input input-error" : "register-input"} onChange={handleFormInput} name="password"/>
+        { formError.password && <p className="error">Please enter a valid password</p>}
       </div>
       <div className="space">
         <label className="register-lable">Select your DOB :</label>
-        <input type="date" className="register-input" onChange={handleFormInput} name="dateOfBirth"/>
+        <input type="date" className={formError.dateOfBirth ? "register-input input-error" : "register-input"}  onChange={handleFormInput} name="dateOfBirth"/>
+        {formError.dateOfBirth && <p className="error">Please selecte a date of birth</p>}
       </div>
       <div className="space">
         <label className="register-lable">Select your Gender :</label>
         <input type="radio" name="gender" onChange={handleFormInput} value="Male"/>Male
         <input type="radio" name="gender" onChange={handleFormInput} value="Female"/>Female
         <input type="radio" name="gender" onChange={handleFormInput} value="Others"/>Others
+        {formError.gender && <p className="error">Please select any one gender</p>}
       </div>
       <div className="space">
         <label className="register-lable">Select your Hobbies :</label>
@@ -54,14 +92,16 @@ const RegisterPage = () => {
         <input type="checkbox" onChange={handleFormInput} value="Hockey" name="hobbies"/>Hockey
         <input type="checkbox" onChange={handleFormInput} value="Chess" name="hobbies"/>Chess
         <input type="checkbox" onChange={handleFormInput} value="HorseRiding" name="hobbies"/>HorseRiding
+        {formError.hobbies && <p className="error">Please select any one hobbies</p>}
       </div>
       <div className="space">
         <label className="register-lable">Enter your Address :</label>
-        <textarea placeholder="Enter your address" className="register-input" onChange={handleFormInput} name="address"></textarea>
+        <textarea placeholder="Enter your address" className={formError.address ? "register-input input-error" : "register-input"}  onChange={handleFormInput} name="address"></textarea>
+        {formError.address && <p className="error">Please enter a valid address</p>}
       </div>
       <div className="space">
         <label className="register-lable">Select your Higher Qualification :</label>
-        <select className="register-input" defaultValue={""} onChange={handleFormInput} name="qualification">
+        <select className={formError.qualification ? "register-input input-error" : "register-input"} defaultValue={""} onChange={handleFormInput} name="qualification">
           <option disabled value="">Please select your qualification</option>
           <option>Under Graduagate</option>
           <option>Post Graduagate</option>
@@ -69,6 +109,7 @@ const RegisterPage = () => {
           <option>Diploma Degree</option>
           <option>Masters Degree</option>
         </select>
+        {formError.qualification && <p className="error">Please select any one qualification</p>}
       </div>
       <div className="space">
         <button onClick={() => submitRegister()}>Create your Account</button>
