edc8d904ed9e8e243059c91ec3212282b66ce122
kvelraj_concord <kvelraj@concord.net>
2022-11-05 12:32:15 +0530

    get api

--- learning-react/src/pages/users.js
+++ learning-react/src/pages/users.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 
 const UserPage = () => {
+
+  let [profiles, setProfiles] = useState([]);
 
   const loadProfiles = () => {
    const api_url = "https://reqres.in/api/users?page=2";
@@ -9,7 +11,7 @@ const UserPage = () => {
    axios.get(api_url)
         .then((response) => {
           let serverData = response.data;
-          console.log(serverData)
+          setProfiles(serverData.data);
         })
         .catch((error) => {
           console.log(error);
@@ -22,6 +24,32 @@ const UserPage = () => {
     <div>
       <h1>List of Users</h1>
       <button onClick={() => loadProfiles()}>Load User Profiles</button>
+      <table id="customers">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email ID</th>
+              <th>Profile Pic</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              profiles.map((value, index) => {
+                return(
+                  <tr key={index}>
+                    <td>{value.id}</td>
+                    <td>{value.first_name} {value.last_name}</td>
+                    <td>{value.email}</td>
+                    <td>
+                      <img src={value.avatar} alt="profile" className="avatar"/>
+                    </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+      </table>
     </div>
   )
 }
