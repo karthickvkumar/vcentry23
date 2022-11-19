1f6e138c2f7d8a08b85f1642fbf390408a6201b8
kvelraj_concord <kvelraj@concord.net>
2022-11-19 12:50:47 +0530

    node api

--- NodeAPI/server.js
+++ NodeAPI/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql");
+const e = require("express");
 
 const app = express();
 
@@ -14,6 +15,30 @@ app.use(cors({
 
 const http = require("http").createServer(app);
 
+/*
+Local host XHAMM
+host : "localhost",
+user: "root",
+password: "",
+database: "",
+port: 8080
+*/
+const connection = mysql.createConnection({
+  host : "remotemysql.com",
+  user: "wym4khPjwJ",
+  password: "IpVePeo1GV",
+  database: "wym4khPjwJ",
+  port: 3306
+});
+
+connection.connect((error) => {
+  if(error){
+    throw error;
+  }
+  else{
+    console.log("MySql databse is connected succssfully");
+  }
+})
 
 const port = process.env.PORT || 4000;
 http.listen(port, () => {
