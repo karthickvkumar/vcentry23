b339d8970806dc7623739fc4bff31f1bac482093
kvelraj_concord <kvelraj@concord.net>
2022-11-20 11:12:11 +0530

    node api

--- NodeAPI/server.js
+++ NodeAPI/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql");
-const e = require("express");
 
 const app = express();
 
@@ -38,7 +37,71 @@ connection.connect((error) => {
   else{
     console.log("MySql databse is connected succssfully");
   }
+});
+
+const tableName = "vcentry23";
+const serverError = "Sorry something went wrong in Server, pls contact Admin";
+
+//https:domainName.com/user
+// http:localhost:4000/user 
+app.get("/user", (request, response) => {
+  const sqlQuery = `SELECT * FROM ${tableName}`;
+
+  connection.query(sqlQuery, (error, result) => {
+    if(error){
+      response.status(500).send({
+        actualError : error,
+        message : serverError
+      })
+    }
+    else{
+      response.status(200).send(result);
+    }
+  })
+});
+
+//https:domainName.com/create
+// http:localhost:4000/create 
+app.post("/create", (request, response) => {
+  const name = request.body.name;
+  if(name == "" || name == undefined){
+    response.status(400).send({
+      message: "Invalid Name"
+    });
+    return;
+  }
+  const age = request.body.age;
+  if(age == undefined || age == "" || typeof age != 'number'){
+    response.status(400).send({
+      message: "Invalid Age"
+    });
+    return;
+  }
+  const email = request.body.email;
+  const location = request.body.location;
+  const mobileNumber = request.body.mobile;
+
+  const sqlQuery = `INSERT INTO ${tableName} (name, age, email, location, mobile_number) VALUES ('${name}', ${age}, '${email}', '${location}', ${mobileNumber})`;
+
+  connection.query(sqlQuery, (error, result) => {
+    if(error){
+      response.status(500).send({
+        actualError : error,
+        message : serverError
+      })
+    }
+    else{
+      response.status(200).send({
+        actualResult : result,
+        message : "Successfully user profile has been saved"
+      });
+    }
+  })
+  
 })
+
+
+
 
 const port = process.env.PORT || 4000;
 http.listen(port, () => {
